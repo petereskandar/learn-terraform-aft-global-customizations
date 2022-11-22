101234746b3b994e972ca8267fbd92c0ad47e7f0
Peter Eskandar <peter.eskandar@posteitaliane.it>
2022-11-22 23:48:16 +0100

    demo default security group

---
+++ terraform/main.tf
@@ -0,0 +1,32 @@
+resource "aws_default_vpc" "default" {
+  tags = {
+    Name = "Default VPC - Control Tower"
+  }
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_default_vpc.default.id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = [aws_default_vpc.default.cidr_block]
+    ipv6_cidr_blocks = [aws_default_vpc.default.ipv6_cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
