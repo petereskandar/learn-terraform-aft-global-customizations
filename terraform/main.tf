4917027c11b1514a07335dedc70cc4c90265e774
Peter Eskandar <peter.eskandar@posteitaliane.it>
2022-11-23 03:10:53 +0100

    update security group

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,7 +14,7 @@ resource "aws_security_group" "allow_tls" {
     from_port        = 443
     to_port          = 443
     protocol         = "tcp"
-    cidr_blocks      = [aws_default_vpc.default.cidr_block]
+    cidr_blocks      = ["10.0.0.0/8"]
   }
 
   egress {
