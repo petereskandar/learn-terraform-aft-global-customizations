c32e412c367eb49f83ebd67300b5be4e53def253
Peter Eskandar <peter.eskandar@posteitaliane.it>
2022-11-24 02:45:58 +0100

    update SG ingress rule

--- terraform/main.tf
+++ terraform/main.tf
@@ -50,8 +50,7 @@ resource "aws_security_group" "default-sg-eu-west-3" {
     from_port         = 0
     to_port           = 0
     protocol          = "-1"
-    self              = true
-    security_groups   = [aws_security_group.default-sg-eu-west-3.id]
+    cidr_blocks = ["10.0.0.0/8"]
   }
 
   egress {
