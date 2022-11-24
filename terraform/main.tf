c8d5f06e088cdaf5b503a350aefc26ab8a6ed509
Peter Eskandar <peter.eskandar@posteitaliane.it>
2022-11-24 02:41:29 +0100

    update ingress

--- terraform/main.tf
+++ terraform/main.tf
@@ -51,7 +51,7 @@ resource "aws_security_group" "default-sg-eu-west-3" {
     to_port           = 0
     protocol          = "-1"
     self              = true
-    security_group_id = aws_security_group.default-sg-eu-west-3.id
+    security_groups   = [aws_security_group.default-sg-eu-west-3.id]
   }
 
   egress {
